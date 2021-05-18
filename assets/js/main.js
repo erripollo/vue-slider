44fa88c550c18305ab9fc29e98f5025cfa2eac20
erripollo <pollone.enrico@gmail.com>
2021-05-18 18:11:47 +0200

    Create slider

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        counter: 0,
+        images: [
+            './assets/img/slide-1.jpeg',
+            './assets/img/slide-2.jpeg',
+            './assets/img/slide-3.jpeg',
+            './assets/img/slide-4.jpeg',
+            './assets/img/slide-5.jpeg',
+            './assets/img/slide-6.jpeg',
+            './assets/img/slide-7.jpeg',
+            './assets/img/slide-8.jpeg'
+        ],
+
+    },
+
+    methods: {
+        prev(){
+            if (this.counter === 0){
+                return this.counter = this.images.length - 1;
+            }
+            return this.counter -= 1
+        },
+
+        next(){
+            if (this.counter === this.images.length - 1){
+                return this.counter = 0;
+            }
+            return this.counter += 1;
+        },
+    }
+
+
+})
